53d18784f1ffd9abfcaa971d76450d1482de9782
Alexander Paglia <alexp06@my.yorku.ca>
2024-03-07 17:30:34 -0500

    Quiz Scene
    Added the quiz management

---
+++ AR Study Pod/Assets/Scripts/Answers.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Answers : MonoBehaviour
+{
+    public bool isCorrect = false;
+    public QuizManager quizManager;
+    public Color startColor;
+
+    private void Start()
+    {
+        startColor = GetComponent<Image>().color;
+    }
+    public void Answer()
+    {
+        if (isCorrect)
+        {
+            GetComponent<Image>().color = Color.green;
+            Debug.Log("Correct Answer");
+            quizManager.correct();
+            
+        }
+        else
+        {
+            GetComponent<Image>().color = Color.red;
+            Debug.Log("Incorrect Answer");
+            quizManager.wrong();
+        }
+    }
+}
